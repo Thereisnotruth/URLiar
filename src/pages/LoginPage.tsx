5c83d849e9cadbdfb65c801314f2be6899cdde3f
고태진 <thereisnotruth12@gmail.com>
2023-02-22 17:08:10 +0900

    comment

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -12,23 +12,28 @@ const LoginPage: React.FC = () => {
         <div className="fill-urliar-2 h-[85px]">
           <Logo className="w-[100%]" />
         </div>
-        <div className="bg-urliar-3 w-[400px] h-[380px] mx-auto rounded-[10px] p-[20px]">
+        {/* 로그인 폼 */}
+        <form className="bg-urliar-3 w-[400px] h-[380px] mx-auto rounded-[10px] p-[20px]">
+          {/* 이메일 입력 창 */}
           <input
             type="text"
             className="w-[330px] h-[40px] rounded-[10px] my-[10px] px-[10px]"
             placeholder="이메일"
           />
+          {/* 비밀번호 입력 창 */}
           <input
             type="password"
             className="w-[330px] h-[40px] rounded-[10px] my-[10px] px-[10px]"
             placeholder="비밀번호"
           />
+          {/* 로그인 버튼 */}
           <button
-            type="button"
+            type="submit"
             className="bg-urliar-2 w-[330px] h-[50px] rounded-[10px] text-[20px] text-[#ffffff] my-[10px]"
           >
             로그인
           </button>
+          {/* 회원가입 버튼 */}
           <button
             type="button"
             className="bg-urliar-2 w-[330px] h-[50px] rounded-[10px] text-[20px] text-[#ffffff] my-[10px]"
@@ -36,7 +41,7 @@ const LoginPage: React.FC = () => {
           >
             회원가입
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
