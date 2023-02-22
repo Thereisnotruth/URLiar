a247b28abbb37d93c1b75a91049b44bc810bd6e7
고태진 <thereisnotruth12@gmail.com>
2023-02-22 17:04:52 +0900

    login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,16 +9,34 @@ const LoginPage: React.FC = () => {
       {/* 로그인 페이지 */}
       <div className="bg-urliar-1 w-[500px] h-[520px] mx-auto rounded-[10px]">
         {/* 로그인 창 */}
-        <div className="text-urliar-3 fill-urliar-3 stroke-urliar-3">
+        <div className="fill-urliar-2 h-[85px]">
           <Logo className="w-[100%]" />
         </div>
-        <button
-          type="button"
-          className="bg-urliar-3 mx-auto"
-          onClick={() => navigate('/register')}
-        >
-          회원가입
-        </button>
+        <div className="bg-urliar-3 w-[400px] h-[380px] mx-auto rounded-[10px] p-[20px]">
+          <input
+            type="text"
+            className="w-[330px] h-[40px] rounded-[10px] my-[10px] px-[10px]"
+            placeholder="이메일"
+          />
+          <input
+            type="password"
+            className="w-[330px] h-[40px] rounded-[10px] my-[10px] px-[10px]"
+            placeholder="비밀번호"
+          />
+          <button
+            type="button"
+            className="bg-urliar-2 w-[330px] h-[50px] rounded-[10px] text-[20px] text-[#ffffff] my-[10px]"
+          >
+            로그인
+          </button>
+          <button
+            type="button"
+            className="bg-urliar-2 w-[330px] h-[50px] rounded-[10px] text-[20px] text-[#ffffff] my-[10px]"
+            onClick={() => navigate('/register')}
+          >
+            회원가입
+          </button>
+        </div>
       </div>
     </div>
   );
