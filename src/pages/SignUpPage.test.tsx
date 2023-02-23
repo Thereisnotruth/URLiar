3ee0775aef27c8dbe60c1e79e12464125d899b00
고태진 <thereisnotruth12@gmail.com>
2023-02-23 11:02:50 +0900

    sign up

--- src/pages/SignUpPage.test.tsx
+++ src/pages/SignUpPage.test.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import SignUpPage from './SignUpPage';
 
 describe('SignUpPage', () => {
-  it('should navigate to main page when return button is clicked', () => {
+  it('navigate to main page when return button is clicked', () => {
     // Render component
     render(<SignUpPage />, { wrapper: BrowserRouter });
 
@@ -15,5 +15,44 @@ describe('SignUpPage', () => {
 
     // Assert that browser navigated to the expected URL
     expect(window.location.pathname).toBe('/');
+  });
+
+  it('enable button when email, password, password check, nickname are entered', () => {
+    render(<SignUpPage />, { wrapper: BrowserRouter });
+
+    const email = screen.getByPlaceholderText('email');
+    const pw = screen.getByPlaceholderText('password');
+    const pwChk = screen.getByPlaceholderText('password check');
+    const nickname = screen.getByPlaceholderText('nickname');
+    const button = screen.getByRole('button', {
+      name: '회원가입',
+    });
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(email, { target: { value: 'user@test.com' } });
+    fireEvent.change(pw, { target: { value: 'test1234' } });
+    fireEvent.change(pwChk, { target: { value: 'test1234' } });
+    fireEvent.change(nickname, { target: { value: 'test' } });
+    expect(button).toBeEnabled();
+  });
+
+  it('disable button when password and password check have different value', () => {
+    render(<SignUpPage />, { wrapper: BrowserRouter });
+
+    const email = screen.getByPlaceholderText('email');
+    const pw = screen.getByPlaceholderText('password');
+    const pwChk = screen.getByPlaceholderText('password check');
+    const nickname = screen.getByPlaceholderText('nickname');
+    const button = screen.getByRole('button', {
+      name: '회원가입',
+    });
+
+    fireEvent.change(email, { target: { value: 'user@test.com' } });
+    fireEvent.change(pw, { target: { value: 'test12345' } });
+    fireEvent.change(pwChk, { target: { value: 'test1234' } });
+    fireEvent.change(nickname, { target: { value: 'test' } });
+
+    expect(button).toBeDisabled();
   });
 });
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState<string>('');
+  const [pw, setPw] = useState<string>('');
+  const [pwChk, setPwChk] = useState<string>('');
+  const [nickname, setNickname] = useState<string>('');
+
   const navigate = useNavigate();
+
   return (
     <div>
       <button type="button" role="moveBack" onClick={() => navigate('/')}>
         뒤로가기
+      </button>
+      <input
+        type="text"
+        placeholder="email"
+        onChange={({ target: { value } }) => setEmail(value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={({ target: { value } }) => setPw(value)}
+      />
+      <input
+        type="password"
+        placeholder="password check"
+        onChange={({ target: { value } }) => setPwChk(value)}
+      />
+      {pw === pwChk ? null : '비밀번호가 일치하지 않습니다.'}
+      <input
+        type="text"
+        placeholder="nickname"
+        onChange={({ target: { value } }) => setNickname(value)}
+      />
+      <button
+        type="button"
+        disabled={!email || !pw || !pwChk || !nickname || pw !== pwChk}
+      >
+        회원가입
       </button>
     </div>
   );
